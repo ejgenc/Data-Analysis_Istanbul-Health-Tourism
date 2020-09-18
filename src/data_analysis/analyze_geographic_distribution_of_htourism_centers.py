295f1f653fc00a9174330444391f6eebc44e971e
ejgenc <33980760+ejgenc@users.noreply.github.com>
2020-09-18 16:08:43 +0300

    Finalized analyze_geographic_distribution_of_htourism_centers.py

--- src/data_analysis/analyze_geographic_distribution_of_htourism_centers.py
+++ src/data_analysis/analyze_geographic_distribution_of_htourism_centers.py
@@ -1,7 +1,73 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 18 15:29:34 2020
+------ What is this file? ------
 
-@author: ejgen
+This script targets three files:
+    - htourism_centers_processed.shp
+    - istanbul_districts.shp
+    - district_income.xlsx
+    
+The script analyzes the geographic distribution of health tourism centers at
+the district level while also adding some extra information for further
+bivariate correlation analysis.
+
+Returns XXXXXXXXXXXXX
 """
+#%% --- Import Required Packages ---
+
+import os
+from pathlib import Path # To wrap around filepaths
+import pandas as pd
+import geopandas as gpd
+#%% --- Set proper directory to assure integration with doit ---
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+#%% --- Import Data ---
+
+#Import airbnb data
+import_fp = Path("../../data/external/istanbul_districts.shp")
+districts_gdf = gpd.read_file(import_fp, encoding = "utf-8-sig")
+
+#Import htourism centers data
+import_fp = Path("../../data/processed/htourism_centers_processed.shp")
+htourism_gdf = gpd.read_file(import_fp, encoding = "utf-8-sig")
+
+import_fp = Path("../../data/external/district_income.xlsx")
+extra_data = pd.read_excel(import_fp)
+
+#%% --- Aggregate htourism center count per district ---
+
+htourism_count_per_district = htourism_gdf.loc[:,"district_e"].value_counts().rename_axis('district_e').reset_index(name='htourism_count')
+
+#%% --- Join in with district data ---
+
+districts_gdf = districts_gdf.merge(htourism_count_per_district,
+                                               on = "district_e",
+                                               how = "left")
+
+#%% --- Fill NA's with 0 ---
+
+districts_gdf.fillna(0, inplace = True)
+
+#%% --- Subset only the info that you might need from districts_gdf ---
+
+columns_to_drop = ["OBJECTID", "Shape_Leng", "Shape_Area",
+                   "continent"]
+
+#%% --- Add in extra data ---
+
+extra_data.rename(columns = {"district_eng" : "district_e"}, inplace = True)
+districts_gdf.drop(columns_to_drop, axis = 1, inplace = True)
+districts_gdf = districts_gdf.merge(extra_data.loc[:,["district_e", "population", "yearly_average_household_income"]],
+                                               on = "district_e",
+                                               how = "left")
+
+#%% --- Export data ---
+
+export_fp = Path("../../data/final/geographic_distribution_of_htourism_centers.shp")
+districts_gdf.to_file(export_fp, encoding = "utf-8")
+
 
