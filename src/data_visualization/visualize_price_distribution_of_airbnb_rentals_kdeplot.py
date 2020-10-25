c108a43cd4fecaff66831fa508acb9c79bde1bea
ejgenc <33980760+ejgenc@users.noreply.github.com>
2020-10-25 22:51:45 +0300

    Working on KDplot visualizations.

--- src/data_visualization/visualize_price_distribution_of_airbnb_rentals_kdeplot.py
+++ src/data_visualization/visualize_price_distribution_of_airbnb_rentals_kdeplot.py
@@ -39,45 +39,56 @@ os.chdir(dname)
 import_fp = Path("../../data/processed/istanbul_airbnb_processed.csv")
 airbnb_df = pd.read_csv(import_fp, encoding = "utf-8-sig")
 
-#%% --- Visualization One: KDplot for raw price data ---
+#%% --- Subset Data ---
+
+#Raw price data
+prices_raw = airbnb_df.loc[:,"price"]
+
+#Price data transformed with log10 (To see the nuances better)
+prices_log10 = np.log10(airbnb_df.loc[:,"price"])
+
+#Price data within +/- 1.5 IQR
+price_iqr = iqr(airbnb_df.loc[:,"price"], axis = 0)
+    #Calculate q3 and q1
+q1 = airbnb_df["price"].quantile(0.25)
+q3 = airbnb_df["price"].quantile(0.75)
+    #Create min and max mask for +/* 1.5 IQR
+min_mask = airbnb_df.loc[:,"price"] >= q1 - (1.5 * price_iqr)
+max_mask = airbnb_df.loc[:,"price"] <= q3 + (1.5 * price_iqr)
+    #Combine masks
+combined_mask = min_mask & max_mask
+    #Create subset
+prices_within_iqr_1point5 = airbnb_df.loc[combined_mask]
+
+
+prices = [prices_raw, prices_log10, prices_within_iqr_1point5]
+#%% --- Calculate Extra Statistics ---
+
+extrastats_dict = {}
+
+for subset in prices:
+    extrastats = None
+    
+    
+#%% --- Visualization One: KDplot for raw and log10 transformed price data ---
 
 with plt.style.context('matplotlib_stylesheet_ejg_fixes'):
     
     # --- Create figure and axes ---
     fig_1 = plt.figure(figsize = (10.80,10.80))
     
-    ax = fig_1.add_subplot(1,1,1)
-
-    sns.distplot(airbnb_df.loc[:,"price"],
-            ax = ax,
-            kde = True)
-    
-#%% --- Visualization Two: KDplot for log10 transformed price data ---
-
-with plt.style.context('matplotlib_stylesheet_ejg_fixes'):
-    
-    # --- Create figure and axes ---
-    fig_2 = plt.figure(figsize = (10.80,10.80))
-    
-    ax = fig_2.add_subplot(1,1,1)
-
-    sns.distplot(np.log10(airbnb_df.loc[:,"price"]),
-            ax = ax,
-            kde = True)
-    
-#%% --- Visualization Three: KDplot for raw and log10 transformed price data ---
-# =============================================================================
-# 
-# with plt.style.context('matplotlib_stylesheet_ejg_fixes'):
-#     
-#     # --- Create figure and axes ---
-#     fig_3 = plt.figure(figsize = (10.80,10.80))
-#     
-#     ax = fig_2.add_subplot(1,1,1)
-# 
-#     sns.distplot(np.log10(airbnb_df.loc[:,"price"]),
-#             ax = ax,
-#             kde = True)
-#     
-# =============================================================================
-
+    i = 0
+    for price_data in prices[:-1]:
+        i += 1
+        
+        ax = fig_1.add_subplot(2,1,i)
+        
+        sns.distplot(price_data,
+                ax = ax,
+                color = "#02b72e",
+                hist_kws = {
+                    "edgecolor" : "black"},
+                kde = True,
+                kde_kws = {
+                    "color" : "black"})    
+#%% --- Visualization Two: KDplot for normalized (+/- 1.5 IQR) price data ---
